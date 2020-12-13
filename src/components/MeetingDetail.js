de0498ba106e2d2a23322fe0b9cf63ee9ff26311
Eskimo <kareeemgm@gmail.com>
2020-12-13 16:45:34 +0200

    c

--- src/components/MeetingDetail.js
+++ src/components/MeetingDetail.js
@@ -1,39 +1,14 @@
 import React, { Component } from 'react';
-import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import Box from '@material-ui/core/Box'
 import { Button, List, Paper, TextField, Typography  } from '@material-ui/core';
 import IconButton from '@material-ui/core/IconButton';
 import DeleteIcon from "@material-ui/icons/Delete";
 
-const useStyles = makeStyles((theme) => ({
-    root: {
-       marginRight:0,
-       marginLeft:20,
-       textAlign:'center',
-      
-        
-    },
-    paper: {
-      padding: theme.spacing(1),
-      textAlign: 'center',
-      color: theme.palette.text.primary,
-    },
-    text: {
-        "& .MuiOutlinedInput-root .MuiOutlinedInput-notchedOutline": {
-          borderColor: "green"
-        },
-        "&:hover .MuiOutlinedInput-root .MuiOutlinedInput-notchedOutline": {
-          borderColor: "red"
-        },
-        "& .MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-notchedOutline": {
-          borderColor: "purple"
-        }
-      }
-  }));
+
   
 function RenderMeeting({meetings,delMeeting}){
-    const classes = useStyles();
+  
     if (meetings.length !== 0){
         const metings = meetings.map((meeting) => {
             return(
